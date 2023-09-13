7eda151f7e7ea0fe119860b5e5894595f549f71b
lew <141259991+dwnas@users.noreply.github.com>
2023-09-13 19:42:26 +0100

    convert deprecated settings

--- imNewHere.ts
+++ imNewHere.ts
@@ -2,29 +2,27 @@ import { definePluginSettings } from "@api/Settings";
 import definePlugin, { OptionType } from "@utils/types";
 
 const settings = definePluginSettings({
-  text {
+  text: {
     type: OptionType.STRING,
     description: "What to replace the \"I'm new here, say hi!\" text with",
-    default: "I'm new here :(",
-    restartNeeded: true
-  }
-  
-})
+    default: "I\'m new here :(",
+    restartNeeded: true,
+}});
 
 export default definePlugin({
-  name: "ImNewHere",
-  description: "Allows for the ability to change the \"I'm new here, say hi!\" text on new joiners",
-  settings,
-  authors: [{
-    id: 589322754470445057n,
-    name: "dwnas"
-  }],
-  patches: [{
-    find: "NEW_MEMBER_BADGE_TOOLTIP_TEXT:",
-    replacement: {
-      match: /"I\'m new here, say hi\!"/,
-      replace: "Vencord.Settings.plugins.ImNewHere.text"
-    }
-  }]
-  
-})
+    name: "ImNewHere",
+    description: "Allows for the ability to change the \"I\'m new here, say hi!\" text on new joiners",
+    authors: [{
+        id: 589322754470445057n,
+        name: "dwnas"
+    }],
+    settings,
+    patches: [{
+        find: "NEW_MEMBER_BADGE_TOOLTIP_TEXT:",
+            replacement: {
+                match: /"I\'m new here, say hi\!"/,
+                replace: "$self.settings.store.text"
+            }
+    }],
+
+});
