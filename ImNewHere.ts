d980918d0cb615311d37c606abd5b37ee9964d96
lew <141259991+dwnas@users.noreply.github.com>
2023-09-13 09:37:16 +0100

    Upload ImNewHere.ts

---
+++ ImNewHere.ts
@@ -0,0 +1,30 @@
+import { definePluginSettings } from "@api/Settings";
+import definePlugin, { OptionType } from "@utils/types";
+
+const settings = definePluginSettings({
+  text {
+    type: OptionType.STRING,
+    description: "What to replace the \"I'm new here, say hi!\" text with",
+    default: "I'm new here :(",
+    restartNeeded: true
+  }
+  
+})
+
+export default definePlugin({
+  name: "ImNewHere",
+  description: "Allows for the ability to change the \"I'm new here, say hi!\" text on new joiners",
+  settings,
+  authors: [{
+    id: 589322754470445057n,
+    name: "dwnas"
+  }],
+  patches: [{
+    find: "NEW_MEMBER_BADGE_TOOLTIP_TEXT:",
+    replacement: {
+      match: /"I\'m new here, say hi\!"/,
+      replace: "Vencord.Settings.plugins.ImNewHere.text"
+    }
+  }]
+  
+})
